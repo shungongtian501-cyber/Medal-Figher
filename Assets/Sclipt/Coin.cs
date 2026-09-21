3e2174daab00767c32786c1d76bf581ae988ab16
宮田瞬 <shungongtian501@gmail.com>
2026-09-22 02:31:54 +0900

    流れ完成

--- Assets/Sclipt/Coin.cs
+++ Assets/Sclipt/Coin.cs
@@ -4,8 +4,6 @@ public class Coin : MonoBehaviour
 {
     private CoinSpawner spawner;
 
-    [SerializeField] private AudioSource _SE;
-
     private void Start()
     {
         spawner = FindFirstObjectByType<CoinSpawner>();
@@ -15,16 +13,7 @@ public class Coin : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
-            spawner.EnableSpawn();
-        }
-
-        if (collision.gameObject.CompareTag("Pin"))
-        {
-
-            if (_SE != null)
-            {
-                _SE.Play();
-            }
+            GameManager.Instance.GameOver();
         }
     }
 }
